cf6a2d516359f4877edd78141de0d83e10fcb252
juanvallejo <juan.vallejo.12@cnu.edu>
2014-01-16 02:29:29 -0500

    added more to the webgl setup

--- webgl/webgl.js
+++ webgl/webgl.js
@@ -1,5 +1,74 @@
+var triangleVertextPositionBuffer;
+var squareVertexPositionBuffer;
+
+function drawScene() {
+	gl.viewport(0,0,gl.viewportWidth,gl.viewportHeight);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	mat4.perspective(45,gl.viewportWidth / gl.viewportHeight,0.1,100.0,pMatrix);
+	mat4.identity(mvMatrix);
+	
+}
+
 function getShader(gl, id) {
-	//to be implemented
+	var shaderScript = document.getElementById(id);
+	if(!shaderScript) {
+		return null;
+	}
+
+	var string = "";
+	var k = shaderScript.firstChild;
+	while(k) {
+		if(k.nodeType == 3) string += k.textContent;
+
+		k = k.nextSibling;
+	}
+
+	var shader;
+	if(shaderScript.type == 'x-shader/x-fragment') shader = gl.createShader(gl.FRAGMENT_SHADER);
+	else if(shaderScript.type == 'x-shader/x-vertex') shader = gl.createShader(gl.VERTEX_SHADER);
+	else return null;
+
+	gl.shaderSource(shader,string);
+	gl.compileShader(shader);
+
+	if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS)) {
+		console.log(gl.getShaderInfoLog(shader));
+		return null;
+	}
+
+	return shader;
+}
+
+function initBuffers() {
+	triangleVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER,triangleVertexPositionBuffer);
+
+	var vertices = [
+		 0.0,  0.1,  0.0,
+		-1.0, -1.0,  0.0,
+		 1.0, -1.0,  0.0
+	];
+
+	gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertices),gl.STATIC_DRAW);
+
+	triangleVertexPositionBuffer.sizeOfItems = 3;
+	triangleVertexPositionBuffer.amountOfItems = 3;
+
+	squareVertextPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER,squareVertexPositionBuffer);
+
+	vertices = [
+		 1.0,  0.1,  0.0,
+		-1.0,  1.0,  0.0,
+		 1.0, -1.0,  0.0
+		-1.0, -1.0,  0.0
+	];
+
+	gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(vertices),gl.STATIC_DRAW);
+
+	squareVertexPositionBuffer.sizeOfItems = 3;
+	squareVertexPositionBuffer.amountOfItems = 4;
 }
 
 function setMatrixUniforms() {
